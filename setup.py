ac966b1576a6703aa3ebaf0c0b0295d4c89a8efe
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-01-19 18:35:31 +0100

    Note on why setup.py is retained

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""Barebones setup.py retained for supporting editable_ installs::
+
+    pip install -e .
+
+.. _editable: https://github.com/pypa/packaging-problems/issues/256#issuecomment-504790004
+
+"""
 from setuptools import setup
 
 
