78a4fb32b6a9d54d3496344587cf2b438607d5e7
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-01-22 20:16:35 +0100

    Hostname config, snakemake api works

--- src/eturb/make.py
+++ src/eturb/make.py
@@ -1,10 +1,11 @@
-from snakemake import snakemake, Batch
+from snakemake import snakemake
 from .util import change_dir
 
 
 class Make:
     def __init__(self, sim):
         self.sim = sim
+        self.path_run = sim.output.path_run
         try:
             self.file = next(
                 f for f in sim.output.get_paths() if f.name == "Snakefile"
@@ -14,23 +15,12 @@ class Make:
                 "Unable to get path of Snakefile via Output class."
             )
 
-    @staticmethod
-    def _generate_batch(rules):
-        """Generate a batch of snakemake rules to execute
+    def list(self):
+        """List rules."""
+        with change_dir(self.path_run):
+            snakemake(self.file, workdir=self.path_run, listrules=True)
 
-        :param rules: Iterable of strings representing snakemake rules.
-        """
-        rules = tuple(rules)
-        nb_batches = len(rules)
-        return (
-            Batch(rulename, idx + 1, nb_batches)
-            for idx, rulename in enumerate(rules)
-        )
-
-    def exec(self, rules):
+    def exec(self, rules=('run',), dryrun=False):
         """Execute snakemake rules in sequence."""
-        batches = self._generate_batch(rules)
-        path_run = self.sim.path_run
-        with change_dir(path_run):
-            for batch in batches:
-                snakemake(self.file, batch)
+        with change_dir(self.path_run):
+            snakemake(self.file, targets=rules, dryrun=dryrun)
