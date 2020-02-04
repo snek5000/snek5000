a5e9b9e229f54fbccbb3fec686a120283180f1af
Ashwin Vishnu <avmo@kth.se>
2020-02-04 14:59:20 +0100

    New module eturb.clusters, fix job submission

---
+++ src/eturb/clusters.py
@@ -0,0 +1,36 @@
+try:
+    from fluiddyn.clusters.snic import ClusterSNIC as Base, _venv
+
+
+    class Cluster(Base):
+        default_project = "snic2019-1-2"
+        cmd_run = "_delete"
+        cmd_run_interactive = "_delete"
+        
+        def __init__(self):
+            super().__init__()
+            self.commands_setting_env = [
+                "ml purge",
+                "ml restore",
+                f"source activate {_venv}",
+                'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$LIBRARY_PATH"',
+                "cd ..", 
+                "source activate.sh",
+                "cd bin/",
+            ]
+
+        def _create_txt_launching_script(self, **kwargs):
+            txt = super()._create_txt_launching_script(**kwargs)
+            return "\n".join(
+                [
+                    line for line in txt.splitlines()
+                    if not line.startswith("_delete")
+                ]
+            )
+
+except ImportError as e:
+    from .log import logger
+    logger.warning(str(e))
+    logger.info("Using local machine as a cluster")
+    from fluiddyn.clusters.local import ClusterLocal as Cluster
+
