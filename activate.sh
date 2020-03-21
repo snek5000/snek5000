66c3af7a87387f898a4dd86c5e18a396eb7284d7
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-03-21 12:06:11 +0100

    Attempts to make CI pass
    Setup develop in GH actions
    
    Tests on pull, docs on push
    
    - tests run on every pull request
    - docs are build on push to master / develop
    
    ref:  https://help.github.com/en/actions/reference/workflow-syntax-for-github-actions
    
    CI badges, rename workflow Docs
    
    Try submodules feature from actions/checkout
    
    Revert "Try submodules feature from actions/checkout"
    
    This reverts commit 7f31ccf09a2eb53256e834b7f2311de42109628c.
    
    Remove accidently commited source code
    
    Doc boundary conditions
    
    Revert "Setup develop in GH actions"
    
    This partially reverts commit 886bd6a80948aa6c8d6e46bb4d3864252d8b6e84.
    
    Source activate before tests on CI
    
    Apt install gfortran openmpi
    
    Fix indentation error in drive2.f
    
    Include all source files from toolbox
    
    Build libgs while generating makefile for abl

--- activate.sh
+++ activate.sh
@@ -3,6 +3,19 @@ export SOURCE_ROOT="$DIR/lib/Nek5000"
 export PATH="$PATH:$SOURCE_ROOT/bin"
 
 alias smake="snakemake"
+# tarball utilities
+function tar-ls() {
+  bsdtar tvf $@
+  # tar -tvf $@ --use-compress-program=zstdmt
+}
+function tar-diff() {
+  local argc=$#
+  if [ $argc -ne 2 ]; then
+    echo "Usage: tar-diff FILE1 FILE2"
+    return
+  fi
+  diff --color=auto <(tar-ls $1) <(tar-ls $2)
+}
 
 if [ -d venv ]; then
   source venv/bin/activate
