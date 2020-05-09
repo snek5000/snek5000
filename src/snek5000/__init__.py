49185036fe34a4d0627659653db37f9c4a1bba79
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-09 08:47:49 +0200

    Add function get_asset for snek5000.assets

--- src/snek5000/__init__.py
+++ src/snek5000/__init__.py
@@ -34,6 +34,13 @@ from .log import logger  # noqa: F401
 
 
 def source_root():
+    """Path of Nek5000 source code."""
     with importlib.resources.path(__name__, "__init__.py") as f:
         root = f.parent
     return os.getenv("SOURCE_ROOT", str((root / "../../lib/Nek5000").absolute()))
+
+
+def get_asset(asset_name):
+    """Fetches path of an asset from subpackage ``snek5000.assets``."""
+    asset = next(importlib.resources.path("snek5000.assets", asset_name).gen)
+    return asset
