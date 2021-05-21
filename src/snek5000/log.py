37bba9b5570eef4347d4c240229570504c45355c
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-05-21 13:32:29 +0200

    Use rich instead of colorlog

--- src/snek5000/log.py
+++ src/snek5000/log.py
@@ -7,31 +7,15 @@ A logger instance (variable :code:`logger`).
 import logging
 import os
 
-logger = logging.getLogger(__name__)
-
-
-# Initialize logging
 try:
-    # Set a nice colored output
-    from colorlog import ColoredFormatter
+    from rich.logging import RichHandler
 
-    formatter = ColoredFormatter(
-        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "red",
-        },
-    )
-    stream = logging.StreamHandler()
-    stream.setFormatter(formatter)
-    logger.addHandler(stream)
+    handler = RichHandler()
 except ImportError:
-    # No color available, use default config
-    logging.basicConfig(format="%(levelname)s: %(message)s")
-    logger.info("Disabling color, you really want to install colorlog.")
+    handler = logging.StreamHandler()
+
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
 
 
 if os.getenv("SNEK_DEBUG"):
