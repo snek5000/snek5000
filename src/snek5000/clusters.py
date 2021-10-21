df1dc1d917a8bae6c293b6bb75f5f40c721ab72d
Ashwin V. Mohanan <dev@fluid.quest>
2021-10-21 23:47:20 +0200

    Implement nproc_available for OAR clusters.
    Also more robust nproc detection for SLURM. Fixes #61

--- src/snek5000/clusters.py
+++ src/snek5000/clusters.py
@@ -4,18 +4,52 @@ import os
 from .log import logger
 
 
-def nproc_available():
-    """Detect maximum number of processors available"""
+def _int_getenv(key, default=0):
+    return int(os.getenv(key, default))
 
-    # Inside job schedulers, number of processors assigened will vary
-    # TODO: extend this loop for other schedulers OAR, PBS etc.
-    for var in ("SLURM_NPROCS",):
-        try:
-            nproc = os.environ[var]
-        except KeyError:
-            continue
-        else:
-            break
+
+def _slurm():
+    """Check different environment variables available within a SLURM job.
+
+    .. seealso::
+
+        https://slurm.schedmd.com/srun.html#SECTION_OUTPUT-ENVIRONMENT-VARIABLES
+
+    """
+    return _int_getenv("SLURM_NPROCS") or (
+        _int_getenv("SLURM_NNODES") * _int_getenv("SLURM_CPUS_ON_NODE")
+    )
+
+
+def _oar():
+    """Equivalent to  the shell command::
+
+    NPROC=$(cat ${OAR_NODEFILE} | wc -l).
+
+    """
+    nodefile = os.getenv("OAR_NODEFILE")
+    if nodefile and os.path.exists(nodefile):
+        with open(nodefile) as file:
+            return len(file.readlines())
+    else:
+        return 0
+
+
+def nproc_available():
+    """Detect maximum number of processors available
+
+    Returns
+    -------
+    int
+
+    """
+    # Inside job schedulers, number of processors assigned will vary
+    # TODO: extend this function for other schedulers PBS etc.
+    cluster_nproc = _slurm() or _oar()
+
+    if cluster_nproc:
+        var = "Cluster environment variables"
+        nproc = cluster_nproc
     else:
         var = "os.cpu_count()"
         nproc = os.cpu_count()
