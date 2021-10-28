6b115079833d278b7ed1212b0fdfafcf0b1e6522
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-10-28 12:08:51 +0200

    Improve output/history_points.py

--- src/snek5000/output/history_points.py
+++ src/snek5000/output/history_points.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import numpy as np
+
 
 class HistoryPoints:
     """History points (Nek5000 output)
@@ -10,20 +12,26 @@ class HistoryPoints:
 
     """
 
-    _tag = "history_points"
-
     def __init__(self, output=None):
         self.output = output
         sim = output.sim
         params = sim.params
         hp_params = params.output.history_points
 
-        self.points = hp_params.points
+        self.coords = hp_params.coords
 
-        if self.points is None:
+        if self.coords is None:
             return
 
-        self.nb_points = len(self.points)
+        self.nb_points = len(self.coords)
+        self.coords = np.array(self.coords)
+        shape = self.coords.shape
+
+        if shape[0] >= params.oper.max.hist or shape[1] != params.oper.dim:
+            raise ValueError(
+                "Shape of params.output.history_points.coords are not compatible."
+                f"Should be (< {params.oper.max.hist}, == {params.oper.dim})"
+            )
 
         self.path_file = (
             Path(output.path_run) / f"{sim.info_solver.short_name}.his"
@@ -31,18 +39,13 @@ class HistoryPoints:
         if self.path_file.exists():
             return
 
-        lines = [f"{self.nb_points} !number of monitoring points"]
-        for xyz in self.points:
-            lines.append(" ".join(str(value) for value in xyz))
-
-        txt_hist = "\n".join(lines) + "\n"
-
         with open(self.path_file, "w") as file:
-            file.write(txt_hist)
+            file.write(f"{self.nb_points} !number of monitoring coords\n")
+            np.savetxt(file, self.coords, fmt="%.7E")
 
     @classmethod
     def _complete_params_with_default(cls, params):
         params.output._set_child(
             "history_points",
-            attribs={"x": None, "y": None, "z": None, "points": None},
+            attribs={"coords": None},
         )
