194ab2a10e74e92f30f3a32bc3bd583e4d64a9cd
Ashwin V. Mohanan <dev@fluid.quest>
2021-11-30 14:23:37 +0100

    Use concurrent.futures to simulate multiple Nek5000 builds

--- tests/test_make.py
+++ tests/test_make.py
@@ -1,9 +1,27 @@
-def test_nek5000_make():
+from concurrent.futures import ProcessPoolExecutor as Pool
+from pathlib import Path
+
+
+def nek5000_build(config):
+    from snek5000.make import _Nek5000Make
+
+    nm = _Nek5000Make()
+    return nm.build(config)
+
+
+def test_nek5000_make(pytestconfig):
     import yaml
 
-    from snek5000.make import _Nek5000Make
+    import snek5000
     from snek5000.output.base import Output
 
-    config = yaml.safe_load(Output.find_configfile().read_text())
-    nm = _Nek5000Make()
-    assert nm.build(config)
+    if pytestconfig.getoption("--runslow"):
+        # To force rebuild of Nek5000
+        (Path(snek5000.get_nek_source_root()) / "bin" / "genbox").unlink()
+
+    snek5000_config = yaml.safe_load(Output.find_configfile().read_text())
+
+    with Pool(max_workers=4) as pool:
+        # Launch simultaneous builds
+        for future in (pool.submit(nek5000_build, snek5000_config) for _ in range(4)):
+            assert future.result()
