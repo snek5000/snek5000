f3c9f1f8b48f51fd4a8648053ba1d3e05c848b73
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-11-09 16:58:10 +0100

    snek5000-config-init console script

---
+++ src/snek5000/config.py
@@ -0,0 +1,33 @@
+"""Utility for configuration files"""
+
+from pathlib import Path
+import os
+from socket import gethostname
+import shutil
+
+
+from snek5000 import get_snek_resource
+
+
+def ensure_config_file():
+    """Ensure that a configuration file is present.
+
+    This function is used by the console script `snek5000-config-init`.
+    """
+    host = os.getenv("SNIC_RESOURCE", os.getenv("GITHUB_WORKFLOW", gethostname()))
+    xdg_config = Path(
+        os.path.expandvars(os.getenv("XDG_CONFIG_HOME", "$HOME/.config"))
+    )
+    configfile_xdg_config_host = xdg_config / f"snek5000/{host}.yml"
+    configfile_xdg_config = xdg_config / "snek5000.yml"
+
+    config_files = [configfile_xdg_config_host, configfile_xdg_config]
+    config_files = [path for path in config_files if path.exists()]
+
+    if not config_files:
+        print("No user config file found")
+        configfile_default = Path(get_snek_resource("default_configfile.yml"))
+        print(f"Copying {configfile_default} to {configfile_xdg_config}")
+        shutil.copyfile(configfile_default, configfile_xdg_config)
+    else:
+        print(f"Found configuration file {config_files[0]}")
