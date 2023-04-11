2ac859a582a16ede53ce08b1de9f5982b19d2a89
Ashwin Vishnu <dev@fluid.quest>
2023-04-11 10:48:34 +0200

    Add tests for snek-make-nek

--- tests/test_make.py
+++ tests/test_make.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 
 import pytest
 
-from snek5000.make import _Nek5000Make, snek_make
+from snek5000.make import _Nek5000Make, snek_make, snek_make_nek
 
 
 def nek5000_build(config):
@@ -51,3 +51,19 @@ def test_snek_make_clean(sim_data, monkeypatch):
     monkeypatch.chdir(sim_data)
     with patch.object(sys, "argv", ["snek-make", "clean"]):
         snek_make()
+
+
+def test_snek_make_list():
+    with patch.object(sys, "argv", ["snek-make-nek", "--list"]):
+        with pytest.raises(SystemExit):
+            snek_make_nek()
+
+
+def test_snek_make_nek_clean_default():
+    with patch.object(sys, "argv", ["snek-make-nek"]):
+        snek_make_nek()
+
+
+def test_snek_make_genmap():
+    with patch.object(sys, "argv", ["snek-make-nek", "bin/genmap"]):
+        snek_make_nek()
